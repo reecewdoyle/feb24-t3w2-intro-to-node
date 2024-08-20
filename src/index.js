2ca07425b0d6d12fed71e83f7c43c20df1dbe47d
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-08-20 19:48:54 +1000

    feat(dice): dice feature implemented

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log("Dice roller app is now running!");
+
+
+/**
+ * Generate a random number between 1 and diceSize, inclusive.
+ * @param {*} diceSize The maximum that the dice can roll. 
+ * @returns {number} The number rolled on the dice. 
+ */
+function rollDice(diceSize = 20){
+    let diceResult = 0;
+
+    diceResult = Math.ceil(Math.random() * diceSize);
+
+    return diceResult;
+}
+
+
+console.log("Random dice roll result is: " + rollDice());
